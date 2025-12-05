b982fb5f52a7ef89dab41eb2894fd6885238c235
andrensenn <andrensenn@gmail.com>
2025-12-05 18:34:30 +0000

    day 3 now working

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,26 +1,29 @@
-
 soma = 0
 bateries = []
-f = open("Day3\in.txt", "r")
+
+f = open("Day3/in.txt", "r")
 for line in f:
-    bateries.append(line)
+    bateries.append(line.strip())
 f.close()
-j = 0
+
 while len(bateries) != 0:
-    cur_bat = bateries.pop(0)
-    num1 = int(cur_bat[0])
-    num2 = int(cur_bat[1])
-    for i in range(2, len(cur_bat)-2):
-        if int(cur_bat[i]) > num1:
-            num1 = int(cur_bat[i])
-            num2 = int(cur_bat[i+1])
-            i +=1
-        elif int(cur_bat[i]) > num2:
-            num2 = int(cur_bat[i])
     
-    if int(cur_bat[-2]) > num2:
-        num2 = int(cur_bat[-2])
-    print(j, num1*10+num2)
-    j += 1
-    soma = soma + num1*10 + num2
+    cur_bat = bateries.pop(0)
+
+    num1 = 0
+    num2 = 0
+
+    for i in range(len(cur_bat) - 1):
+        d1 = int(cur_bat[i])
+
+        d2 = max(int(x) for x in cur_bat[i+1:])
+
+        cand = d1 * 10 + d2
+
+        if cand > num1 * 10 + num2:
+            num1 = d1
+            num2 = d2
+
+    soma += num1 * 10 + num2
+
 print(soma)
